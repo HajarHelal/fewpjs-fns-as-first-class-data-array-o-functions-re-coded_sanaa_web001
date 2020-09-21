3e7664644c1f976ca44db8e67d03b23c1bb56a38
Hajar Helal <hajerhelal44@gmail.com>
2020-09-21 21:19:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ let dogBreed="poodle";
   console.log(`Wake ${dogName} the ${dogBreed}`);
   return `Wake ${dogName} the ${dogBreed}`
 }
+function leashDog(dogName, dogBreed) {
+console.log(`Leash  ${dogName} the ${dogBreed}`);
+return `Leash  ${dogName} the ${dogBreed}`
+}
